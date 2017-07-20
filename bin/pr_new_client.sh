79cee493128664aa57718b264a672879f2ae36d9
Luke W Johnston <lwjohnst@gmail.com>
2017-07-19 22:00:04 -0400

    renamed new_client to be sh

--- bin/pr_new_client.sh
+++ bin/pr_new_client.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-CLIENT_DIR=${client_folder}
+echo "Still in development"
+exit 1
 
+check if config file exists
+
+load in config file
+
+check if master directory, etc of the config variables exist
+
+client_dir=${client_folder}
+
+echo what is the name of the client (no spaces please. recommended would use CamelCase (for example JimmyTimmy))
+read client_name
+
+new_client=${client_folder}/${client_name}
 mkdir ${CLIENT_DIR}/Photos Documents
 mkdir ${CLIENT_DIR}/Photos/{photographer-selection,client-choice}
 touch ${CLIENT_DIR}/Photos/photographer-selection/photo-numbers-from-raw.txt
