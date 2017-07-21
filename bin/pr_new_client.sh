e89776e33f8fd6852abe9eb537353178c6e95f7e
Luke W Johnston <lwjohnst@gmail.com>
2017-07-21 00:05:02 -0400

    first draft of new client command. Last bit still in dev

--- bin/pr_new_client.sh
+++ bin/pr_new_client.sh
@@ -1,30 +1,48 @@
 #!/bin/sh
 
 echo "Still in development"
-exit 1
 
-check if config file exists
+# Check to see if the config file exists and load it if it is.
+if [ ! -f ~/.pr_config.txt ]; then
+    echo "Your configuration file is missing! Ask for help :)"
+else
+    . ~/.pr_config.txt
+fi
 
-load in config file
+# TODO: check if master directory, etc of the config variables exist
 
-check if master directory, etc of the config variables exist
-
-client_dir=${client_folder}
-
-echo what is the name of the client (no spaces please. recommended would use CamelCase (for example JimmyTimmy))
+# Get the name of the client to use as the base for the
+echo "What is the name of the client (no spaces). Recommended use is CamelCase (eg JimmyTimmy)."
 read client_name
 
+# Create the folder directory structure.
 new_client=${client_folder}/${client_name}
-mkdir ${CLIENT_DIR}/Photos Documents
-mkdir ${CLIENT_DIR}/Photos/{photographer-selection,client-choice}
-touch ${CLIENT_DIR}/Photos/photographer-selection/photo-numbers-from-raw.txt
-mkdir ${CLIENT_DIR}/Photos/photographer-selection/{raw,proofs,logo} # Keep logo?
+mkdir $new_client
+mkdir $new_client/Photos/
+mkdir $new_client/Documents/
+mkdir $new_client/Photos/photographer-selection
+mkdir $new_client/Photos/client-choice
+mkdir $new_client/Photos/photographer-selection/logo # Keep logo?
+mkdir $new_client/Photos/photographer-selection/raw
+mkdir $new_client/Photos/photographer-selection/proofs
 
-mkdir ${CLIENT_DIR}/Photos/client-choice/{raw,with-logo,full-res}
-mkdir ${CLIENT_DIR}/Photos/client-choice/raw/{bottom-right,bottom-left,top-right,top-left}
-touch ${CLIENT_DIR}/Photos/client-choice/photo-numbers-from-selection.txt
-touch ${CLIENT_DIR}/Photos/client-choice/photo-numbers-for-branding.txt
+mkdir $new_client/Photos/client-choice/raw
+mkdir $new_client/Photos/client-choice/with-logo
+mkdir $new_client/Photos/client-choice/full-res
+mkdir $new_client/Photos/client-choice/raw/bottom-right
+mkdir $new_client/Photos/client-choice/raw/bottom-left
+mkdir $new_client/Photos/client-choice/raw/top-right
+mkdir $new_client/Photos/client-choice/raw/top-left
+mkdir $new_client/Photos/client-choice/with-logo/bottom-right
+mkdir $new_client/Photos/client-choice/with-logo/bottom-left
+mkdir $new_client/Photos/client-choice/with-logo/top-right
+mkdir $new_client/Photos/client-choice/with-logo/top-left
+touch $new_client/Photos/client-choice/photo-numbers-from-selection.txt
+touch $new_client/Photos/client-choice/photo-numbers-for-branding.txt
 
-cp $TEMPLATE_FOLDER/Contract Documents/
-cp $TEMPLATE_FOLDER/Model-Release-Form Documents/
-touch Documents/visioning
+exit 1
+
+# TODO: check if contract and model release files exist.
+cp $template_folder/Contract Documents/
+cp $template_folder/Model-Release-Form Documents/
+touch Documents/visioning.txt
