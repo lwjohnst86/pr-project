7c879249bd711b99578d2e35b5ab58495b0ef353
Luke W Johnston <lwjohnst@gmail.com>
2017-07-30 14:38:32 -0400

    updated the pr_updated command (untested)

--- bin/pr_update_program.sh
+++ bin/pr_update_program.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
+#
+# Update the commands from the GitHub repository.
+#
+# Usage:
+#
+#   pr_update_program
+#
+echo "Note: Still in development"
 
-echo "Still in development"
-exit 1
+pr_config_load
 
-load the config file
+if [ ! -d "$install_folder" ]; then
+    echo "$install_folder does not exist, did you move the pr-project folder maybe?"
+    exit 1
+fi
 
-pr_dir=${pr_project_directory}
+git -C $install_folder pull origin master
+# Need to update pr_* symbolic links?
+find ~/bin/ -iname "pr_*" -exec chmod 744 {} \;
 
-check if pr_dir exists else exit
-
-git -C ${pr_dir} pull origin master
-newest_tag=$(git describe --tags)
-
-echo Using the latest release ${newest_tag}
-git -C ${pr_dir} checkout tags/${newest_tag}
+# TODO: Use tags? newest_tag=$(git describe --tags)
+#echo Using the latest release ${newest_tag}
+#git -C ${pr_dir} checkout tags/${newest_tag}
