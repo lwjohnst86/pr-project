eba06fe866cc399655c22f50e30b02756d71dae4
Luke W Johnston <lwjohnst@gmail.com>
2017-07-30 14:39:08 -0400

    started steps for moving to choice

---
+++ bin/pr_move_to_choice.sh
@@ -0,0 +1,4 @@
+
+# Read in all numbers (as csv?)
+#  (see http://www.unix.com/shell-programming-and-scripting/31608-read-csv-file-assign-values-variable.html)
+# for each number, move the photo of that number from Photos/photographer-selection/raw into Photos/client-choice/raw
