dcf5ec9b6a6ee5847bb54f46b353b85ca3b5fe89
Luke W Johnston <lwjohnst@gmail.com>
2017-07-30 23:13:27 -0400

    added exit to config_load, checked if vars have been set

--- bin/pr_config_load.sh
+++ bin/pr_config_load.sh
@@ -9,8 +9,28 @@
 if [ ! -f ~/.pr_config.txt ]; then
     echo "Your configuration file is missing! Ask for help :)"
     echo "Or maybe you moved is somewhere else. The file should be called .pr_config.txt."
+    exit 1
 else
     # Loads the options in the config file.
-    . ~/.pr_config.txt
     echo "Loading the configurations file."
+    . ~/.pr_config.txt
+    echo "... Loaded."
+fi
+
+if [ -z "$client_folder" ]; then
+    echo "client_folder option is not set in the .pr_config.txt file."
+    echo "Please fill it out."
+    exit 1
+fi
+
+if [ -z "$template_folder" ]; then
+    echo "template_folder option is not set in the .pr_config.txt file."
+    echo "Please fill it out."
+    exit 1
+fi
+
+if [ -z "$branding_folder" ]; then
+    echo "branding_folder option is not set in the .pr_config.txt file."
+    echo "Please fill it out."
+    exit 1
 fi
