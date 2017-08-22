a4ba41d3eb486cf2f6eae031b52f64901ccdad89
Luke W Johnston <lwjohnst@gmail.com>
2017-08-21 23:11:23 -0400

    added sh to end, added code (incomplete)

---
+++ bin/pr_selection_add_proof.sh
@@ -0,0 +1,12 @@
+
+# Delete all photos in proof
+# Move selection raw into proof
+# add proof to the background
+
+echo "In development"
+exit 1
+
+proofs=$client/Photos/photographer-selection/proofs
+rm -rf $proofs/*
+
+mogrify -p $proofs ... logo again?
