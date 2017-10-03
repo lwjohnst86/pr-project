ce86710caffaf628edad0069068c69c49660ff35
Luke W Johnston <lwjohnst@gmail.com>
2017-10-03 13:46:16 -0400

    update to delete bin commands and add symlinks (non-broken)

--- bin/pr_install.sh
+++ bin/pr_install.sh
@@ -3,21 +3,22 @@
 # (aka for Phoenix Rising).
 #
 # Usage:
-#   sh ~/pr-project/bin/pr_install.sh
+#   sh ~/pr-project/bin/pr_install
 #
 
 # Create a folder called bin.
 mkdir -p ~/bin/
 
-# Make a link of the commands into the bin folder.
-command_folder=$(dirname "$0")
-ln -sf ${command_folder}/*.sh ~/bin/
+# Remove left over pr_* commands
+find ~/bin/ -iname "pr_*" -delete
 
-# TODO? Rename so commands don't have sh?
+# Make a link of the commands into the bin folder.
+command_folder=$(pwd $(dirname "$0"))
+echo ${command_folder}
 
 # Make it so these commands can be executed.
-find ~/bin/ -iname "pr_*" -exec chmod 766 {} \;
-find ~/bin/ -iname "pr_*" -exec rename 's/\.sh$//' {} \;
+find ${command_folder}/ -iname "pr_*" -exec chmod 766 {} \;
+ln -sf ${command_folder}/pr_* ~/bin/
 
 # Put the bin on the search PATH
 if [ -f ~/.profile ]; then
@@ -31,7 +32,7 @@ else
     echo "Please close and reopen your terminal."
 fi
 
-sh $command_folder/pr_config_create.sh
+sh $command_folder/pr_config_create
 
 echo "# Location of the installed git repo
 install_folder=$command_folder" >> ~/.pr_config.txt
